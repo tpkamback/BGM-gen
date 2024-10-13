06ddb442521e314b43efa0fa273885c0c6186f7d
tpkamback <tpkamback@gmail.com>
2024-10-14 01:06:55 +0900

    update log level

--- src/logger_config.py
+++ src/logger_config.py
@@ -2,29 +2,28 @@ import logging
 import time
 import os
 
-# タイムゾーンを日本時間 (JST) に設定
+# set timezone
 os.environ['TZ'] = 'Asia/Tokyo'
 time.tzset()
 
 # ログファイルのパスを指定
 log_file = os.path.join(os.path.dirname(__file__), '../log/youtube_upload.log')
 
-# ロギング設定
 def setup_logger(name=__name__):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
 
-    # フォーマッタの設定
     formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-    # コンソールハンドラ
+    # console handler (INFO)
     console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
-    # ファイルハンドラ
+    # file hundller (DEBUG)
     file_handler = logging.FileHandler(log_file, encoding='utf-8')
-    file_handler.setLevel(logging.DEBUG)    # ファイルはDEBUGレベル
+    file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
