fc2e7dd8458d7f12ae9ba0e36d70062be931af36
tpkamback <tpkamback@gmail.com>
2024-10-20 14:49:06 +0900

    apply USE_LOCALIZATION flag

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ from logger_config import setup_logger
 
 logger = setup_logger(__name__)
 
+USE_LOCALIZATION = False
+
 def main():
     input_root_dir = "./input"
     result_root_dir = "./result"
@@ -26,15 +28,17 @@ def main():
         thumbnail_output = os.path.join(output_dir, "thumbnail_output.png")
         text_img_path = os.path.join(output_dir, "text_img_path.png")
         title_text = "Lofi x Classic"
-    
+
         mp3_files, img_files, prompt = get_inputs_files(input_dir)
         logger.info(f"Fetched MP3 files from directory: {input_dir}")
 
         title, description = get_discprt_from_gpt(prompt)
         logger.info(f"Got title, description.")
 
-        localizations = create_localizations(title, description)
-        logger.info(f"Created localizations.")
+        localizations = None
+        if USE_LOCALIZATION:
+            localizations = create_localizations(title, description)
+            logger.info(f"Created localizations.")
 
         logger.info(f"creating video ...")
         create_video(mp3_files, merged_audio_file, img_files[0], video_output_file, text_img_path, thumbnail_output, title_text)
