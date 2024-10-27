6090ea8305ab1c2d1e5b68f9d37e79a77e3bf15e
tpkamback <tpkamback@gmail.com>
2024-10-27 17:04:59 +0900

    update run_tests precommit

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "Starting the test script..."
+# run pytests
+./docker/run.sh t
+RESULT=$?
+
+if [ $RESULT -ne 0 ]; then
+    echo "Tests failed with exit code $RESULT"
+    exit 1
+fi
+
 echo "Tests passed."
 exit 0
