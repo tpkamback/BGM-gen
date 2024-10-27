109810cf70c90e67380ab680421705a32ec831c3
tpkamback <tpkamback@gmail.com>
2024-10-27 16:53:18 +0900

    add precommit tests

---
+++ scripts/run_tests.sh
@@ -0,0 +1,15 @@
+# run_tests.sh
+
+#!/bin/bash
+
+# run pytests
+./docker/run.sh t
+RESULT=$?
+
+if [ $RESULT -ne 0 ]; then
+    echo "Tests failed with exit code $RESULT"
+    exit 1
+fi
+
+echo "Tests passed."
+exit 0
