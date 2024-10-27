c51cef42f00f60f0b257b57d02b89f610f140dab
tpkamback <tpkamback@gmail.com>
2024-10-27 15:20:08 +0900

    add test_get_thumbnail_files

--- test/test_modify_video.py
+++ test/test_modify_video.py
@@ -11,5 +11,13 @@ def test_get_mp3files_from_download(directory, expected):
     grouped_files = get_mp3files_from_download(directory)
     assert grouped_files == expected
 
+
+@pytest.mark.parametrize("directory, expected", [
+    ("./test/test_gen_data",["./test/test_gen_data/test_thumbnail_0.png", "./test/test_gen_data/test_thumbnail_1.jpg"]),
+])
+def test_get_thumbnail_files(directory, expected):
+    grouped_files = get_thumbnail_files(directory)
+    assert grouped_files == expected
+
 if __name__ == "__main__":
     unittest.main()
