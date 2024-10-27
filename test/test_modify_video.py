212bd37e6dac84c31f24d4ebc34b54c65d7da591
tpkamback <tpkamback@gmail.com>
2024-10-27 15:32:48 +0900

    change test data dir

--- test/test_modify_video.py
+++ test/test_modify_video.py
@@ -5,15 +5,14 @@ from unittest.mock import patch, MagicMock
 from modify_video import get_mp3files_from_download, get_thumbnail_files, create_video, move_files
 
 @pytest.mark.parametrize("directory, expected", [
-    ("./test/test_gen_data", {"test_gen_music": ["./test/test_gen_data/[suno]test_gen_music.mp3"]}),
+    ("./test/test_modify_video", {"test_gen_music": ["./test/test_modify_video/[suno]test_gen_music.mp3"]}),
 ])
 def test_get_mp3files_from_download(directory, expected):
     grouped_files = get_mp3files_from_download(directory)
     assert grouped_files == expected
 
-
 @pytest.mark.parametrize("directory, expected", [
-    ("./test/test_gen_data",["./test/test_gen_data/test_thumbnail_0.png", "./test/test_gen_data/test_thumbnail_1.jpg"]),
+    ("./test/test_modify_video",["./test/test_modify_video/test_thumbnail_0.png", "./test/test_modify_video/test_thumbnail_1.jpg"]),
 ])
 def test_get_thumbnail_files(directory, expected):
     grouped_files = get_thumbnail_files(directory)
